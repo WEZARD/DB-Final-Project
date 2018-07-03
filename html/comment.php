19e098ca9506ce0e60296f744982f61d4c49aabc
Yizhou <414506550@qq.com>
2018-07-03 12:19:18 -0400

    Update comment.php

--- html/comment.php
+++ html/comment.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$dbc = mysqli_connect('127.0.0.1','root','2012211338','Project');
+$dbc = mysqli_connect('127.0.0.1','root','XXXXXX','Project');
 
 $user_name = $_SESSION['user_name'];
 
@@ -11,7 +11,7 @@ date_default_timezone_set('America/New_York');
 $localtime = date("Y-m-d H:i:s");
 
 $comment_query = "INSERT INTO Comment VALUES('$user_name', $proid, '$comment', '$localtime')";
-mysqli_query($dbc,$comment_query);
+mysqli_query($dbc, $comment_query);
 
 
 echo "<script>alert('Comment Successfully!'); window.location.href='project-detail.php?proid=";
