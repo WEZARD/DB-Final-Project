e3b3d83a9b3d320e57eeb9ab5774bd669b5e152f
Yizhou <414506550@qq.com>
2018-07-03 17:49:22 -0400

    Update follow.php

--- html/follow.php
+++ html/follow.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$dbc = mysqli_connect('127.0.0.1','root','2012211338','Project');
+$dbc = mysqli_connect('127.0.0.1','root','XXXXXX','Project');
 
 $user_name = $_SESSION['user_name'];
 $var_follow = $_GET['para'];
@@ -9,7 +9,7 @@ $proid = $_GET['proid'];
 
 if($var_follow == 'unfollow'){
 	$unfollow_query = "DELETE FROM Follow where login_name1 = '$user_name' and login_name2 = '$login_name2'";
-	mysqli_query($dbc,$unfollow_query);
+	mysqli_query($dbc, $unfollow_query);
 	//echo "<script>alert('Follow'); history.go(-2);</script>";
 	$home_url = "project-detail.php?proid=$proid";
     header('Location: '.$home_url);
@@ -17,7 +17,7 @@ if($var_follow == 'unfollow'){
 	
 else{
 	$follow_query = "INSERT INTO Follow VALUES('$user_name','$login_name2')";
-	mysqli_query($dbc,$follow_query);
+	mysqli_query($dbc, $follow_query);
 	//echo "<script>alert('Follow'); history.go(-2);</script>";
 	$home_url = "project-detail.php?proid=$proid";
     header('Location: '.$home_url);
