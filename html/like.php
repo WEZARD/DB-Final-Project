c78e6e5dc125104a15e47015204d06a080897979
Yizhou <414506550@qq.com>
2018-07-03 17:58:18 -0400

    Update like.php

--- html/like.php
+++ html/like.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$dbc = mysqli_connect('127.0.0.1','root','2012211338','Project');
+$dbc = mysqli_connect('127.0.0.1','root','XXXXXX','Project');
 
 $user_name = $_SESSION['user_name'];
 $var_like = $_GET['para'];
@@ -8,14 +8,14 @@ $proid = $_GET['proid'];
 
 if($var_like == 'unlike'){
 	$unlike_query = "DELETE FROM `Like` where login_name = '$user_name' and proid = '$proid'";
-	mysqli_query($dbc,$unlike_query);
+	mysqli_query($dbc, $unlike_query);
 	$home_url = "project-detail.php?proid=$proid";
     header('Location: '.$home_url);
 }
 	
 else{
 	$like_query = "INSERT INTO `Like` VALUES('$user_name','$proid')";
-	mysqli_query($dbc,$like_query);
+	mysqli_query($dbc, $like_query);
 	//echo "<script>alert('Follow'); history.go(-2);</script>";
 	$home_url = "project-detail.php?proid=$proid";
     header('Location: '.$home_url);
