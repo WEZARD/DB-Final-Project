2c8daccf3f691383212dcc4304647fa1c5007719
Yizhou <414506550@qq.com>
2018-07-03 15:17:03 -0400

    Update create.php

--- html/create.php
+++ html/create.php
@@ -2,7 +2,7 @@
 session_start();
   
   if(isset($_POST['submit'])){
-    $dbc = mysqli_connect('127.0.0.1','root','2012211338','Project');
+    $dbc = mysqli_connect('127.0.0.1','root','XXXXXX','Project');
     
     $project_name = mysqli_real_escape_string($dbc,trim($_POST['pname']));
     $project_description = mysqli_real_escape_string($dbc,trim($_POST['pdescription']));
@@ -17,11 +17,11 @@ session_start();
     $posttime = date("Y-m-d H:i:s");
 
     $query1 = "SELECT proname FROM Projects ";
-    $data1 = mysqli_query($dbc,$query1);
+    $data1 = mysqli_query($dbc, $query1);
     $pnum = mysqli_num_rows($data1) + 1;
 
        $query_reg = "INSERT INTO Projects VALUES('$pnum','$project_name','$project_description',NULL,'$project_category','$project_loginname','$project_minfunds','$project_maxfunds','0','$posttime','$project_endtime','$project_plantime','funding',NULL)";
-          mysqli_query($dbc,$query_reg);
+          mysqli_query($dbc, $query_reg);
 
           $home_url = 'usercenter-project.php';
           header('Location: '.$home_url);
